e6c616ca2a85eba46e31ffc1dbff80e81791b0d7
hemanthbm2003 <bmhemanth893@gmail.com>
2024-10-25 13:38:43 +0530

    Add files via upload
    added html css and javascript file

---
+++ list.js
@@ -0,0 +1,25 @@
+document.getElementById("addTaskBtn").addEventListener("click",function(){
+    const taskInput=document.getElementById("taskInput");
+    const taskText=taskInput.value.trim();
+
+    if (taskText !=="") {
+        addTask(taskText);
+        taskInput.value = "";
+    }
+});
+
+function addTask(task) {
+    const taskList = document.getElementById("taskList");
+    const li = document.createElement("li");
+
+    li.textContent = task;
+    
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener("click", function() {
+        taskList.removeChild(li);
+    });
+
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+}
